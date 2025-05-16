054eea19d4459800b22a0b412b1cbdb54614fa99
Oleg Katkov <lezh1k.vohrer@gmail.com>
2025-05-16 12:58:26 +0600

    Changed telescope tag to branch 0.1.x

--- nvim/lua/lezh1k/lazy.lua
+++ nvim/lua/lezh1k/lazy.lua
@@ -29,8 +29,8 @@ require("lazy").setup({
 
   {
     "nvim-telescope/telescope.nvim",
-    tag = '0.1.8',
-    -- branch = "0.1.x",
+    -- tag = '0.1.8',
+    branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
 
