826123e66f6fef4163c5d4b6ce4d0a164997ff26
Oleg Katkov <lezh1k.vohrer@gmail.com>
2025-07-29 11:34:21 +0600

    Changed settings, added actual i3blocks

--- nvim/lua/lezh1k/lazy.lua
+++ nvim/lua/lezh1k/lazy.lua
@@ -46,7 +46,9 @@ require("lazy").setup({
 
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    branch = 'main',
+    lazy = false,
+    build = ":TSUpdate"
   },
 
   { "NLKNguyen/papercolor-theme" },
@@ -58,16 +60,16 @@ require("lazy").setup({
   { "ray-x/lsp_signature.nvim" },
 
   {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
+    "mason-org/mason.nvim",
+    "neovim/nvim-lspconfig",
   },
 
   -- nvim-cmp
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/cmp-buffer'},
-  {'hrsh7th/cmp-path'},
-  {'hrsh7th/cmp-cmdline'},
-  {'hrsh7th/nvim-cmp'},
-  {'hrsh7th/cmp-vsnip'},
-  {'hrsh7th/vim-vsnip'},
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  { 'hrsh7th/cmp-cmdline' },
+  { 'hrsh7th/nvim-cmp' },
+  { 'hrsh7th/cmp-vsnip' },
+  { 'hrsh7th/vim-vsnip' },
 })
