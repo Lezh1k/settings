057968a5df9a491f853a05cf2640f94533144eb1
Oleg Katkov <lezh1k.vohrer@gmail.com>
2025-09-24 12:04:18 +0600

    Added xrandr configs

---
+++ screen/big.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 2048x1152 --pos 0x0 --rotate normal --output eDP-1-1 --off
---
+++ screen/small.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --off --output eDP-1-1 --primary --mode 1368x768 --pos 0x0 --rotate normal
