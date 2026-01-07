c026190c25de6415825a75682cda11739c84bbbd
lezh1k <lezh1k@lezh1k.bsd>
2026-01-07 14:59:54 +0600

    Changed batt.sh for freebsd + rearranged files

--- i3/i3blocks/freebsd/battery/batt.sh
+++ i3/i3blocks/freebsd/battery/batt.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-sysctl hw.acpi.battery.life | awk '{print ENVIRON["LABEL"] $2"%"}'
+LABEL=⚡
+sysctl -n hw.acpi.battery.life | awk '
+{
+    red = "#ff0000"
+    yellow = "#ffff00"
+    green = "#00ff00"
+
+    p = $1 + 0
+    if (p < 15)
+        c = red
+    else if (p < 80)
+        c = yellow
+    else
+        c = green
+
+    printf("{\"full_text\":\"⚡%d%%\",\"color\":\"%s\"}\n", p, c)
+}'
+
